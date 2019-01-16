2b953767c8d254ee138b6a19b82004db1909e484
cristeaioan <c.ioan12@yahoo.com>
2019-01-16 16:45:53 +0200

    asd

--- js/main.js
+++ js/main.js
@@ -22,58 +22,95 @@ jQuery(function($) {
 			return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 		}
 
+
 		// Facem un apel catre API-ul de la Geonames pentru a obtine informatii
 		// legate de locatie (codul tarii, numele tarii si numele orasului)
 		// cu ajutorul latitudinii si longitudinii
-		$.getJSON('https://secure.geonames.org/countrySubdivisionJSON?lat=' + lat + '&lng=' + lon + '&username=cristeaioan', function(locInfo) {
-			// Setam unitatea de masura specifica tarii (metric/imperial)
-			var impCountryCodes = ['BS', 'BZ', 'KY', 'PW', 'US'],
-				units;
-			if(impCountryCodes.indexOf(locInfo.countryCode) === -1) {
-				units='si';
-			} else {
-				units='us';
-			}
+		$.ajax({
+			type: 'GET',
+			url: 'http://secure.geonames.org/countrySubdivision?lat=' + lat + '&lng=' + lon + '&username=cristeaioan',
+			dataType: 'xml',
+			success: function (xml) {
+				// Setam unitatea de masura specifica tarii (metric/imperial)
+				var impCountryCodes = ['BS', 'BZ', 'KY', 'PW', 'US'],
+					units;
+				if(impCountryCodes.indexOf($(xml).find('countryCode')) === -1) {
+					units='metric';
+				} else {
+					units='imperial';
+				}
 
-			// Functie pentru afisarea unitatii de masura pentru temperatura
-			function unitText() {
-				if (units === 'si') return ' &deg;C'; else return ' &deg;F';
-			}
+				// Functie pentru afisarea unitatii de masura pentru temperatura
+				function unitText() {
+					if (units === 'metric') return ' &deg;C'; else return ' &deg;F';
+				}
+
+				// Afisam numele orasului
+				$('.location h2').html($(xml).find('adminName1'));
+				// Afisam numele tarii
+				$('.location h3').html($(xml).find('countryName'));
 
 
-			// Afisam numele orasului
-			$('.location h2').html(locInfo.adminName1);
-			// Afisam numele tarii
-			$('.location h3').html(locInfo.countryName);
+				// Facem un apel catre API-ul de la Darksky pentru a obtine informatii
+				// legate de vreme cu ajutorul latitudinii si a longitudinii
+				// Precizam unitatea de masura
+				$.ajax({
+					type: 'GET',
+					url: 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat +'&lon=' + lon + '&mode=xml&units=' + units + '&appid=fcb6592fbcf59c296c70c45a0ed72a58',
+					dataType: 'xml',
+					success: function (xml) {
+						console.log('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +'&lon=' + lon + '&mode=xml&units=' + units + '&appid=fcb6592fbcf59c296c70c45a0ed72a58');
 
+						$('.curr-wthr .container').css('background-image', 'url("images/bgs/' + $(xml).find('weather').attr('icon') + '.jpg")');
 
-			// Facem un apel catre API-ul de la Darksky pentru a obtine informatii
-			// legate de vreme cu ajutorul latitudinii si a longitudinii
-			// Precizam unitatea de masura
-			$.get('https://api.darksky.net/forecast/b421d301ce7ef50d4a9d6e02ff0373ed/' + lat +',' + lon + '?units=' + units, function(weatherInfo) {
-				$('.curr-wthr .container').css('background-image', 'url("images/bgs/' + weatherInfo.currently.icon + '.jpg")');
+						// Afisam temperatura actuala
+						$('.curr-wthr .temp h1').html(Math.floor($(xml).find('temperature').attr('value')) + '<span>' + unitText() + '</span>');
 
-				// Afisam temperatura actuala
-				$('.curr-wthr .temp h1').html(Math.floor(weatherInfo.currently.temperature) + '<span>' + unitText() + '</span>');
-
-				// Afisam icoana specifica prognozei actuale
-				$('.curr-wthr-state .icon img').attr('src', 'images/icons/' + weatherInfo.currently.icon + '.svg');
-				$('.curr-wthr-state .description').html(upper(weatherInfo.currently.summary));
-
-				// Afisam prognoza meteo pentru urmatoarele zile
-				$('.daily .day').each(function(day) {
-					day++;
-
-					// Afisam ziua saptamanii
-					$(this).children('.heading').html(time(weatherInfo.daily.data[day].time));
-					// Afisam icoana specifica prognozei pe acea zi
-					$(this).children('.icon').children('img').attr('src', 'images/icons/' + weatherInfo.daily.data[day].icon + '.svg');
-					// Calculam si afisam temperatura media in acea zi
-					$(this).children('.temp').html(Math.floor((weatherInfo.daily.data[day].temperatureMin+weatherInfo.daily.data[day].temperatureMax)/2) + unitText());
+						// Afisam icoana specifica prognozei actuale
+						$('.curr-wthr-state .icon img').attr('src', 'images/icons2/' + $(xml).find('weather').attr('icon') + '.svg');
+						$('.curr-wthr-state .description').html(upper($(xml).find('weather').attr('value')));
+						//
+						// // Afisam prognoza meteo pentru urmatoarele zile
+						// $('.daily .day').each(function(day) {
+						// 	day++;
+						//
+						// 	// Afisam ziua saptamanii
+						// 	$(this).children('.heading').html(time($(xml).find('daily').find('data[' + day + ']').find('time')));
+						// 	// Afisam icoana specifica prognozei pe acea zi
+						// 	$(this).children('.icon').children('img').attr('src', 'images/icons/' + $(xml).find('daily').find('data[' + day + ']').find('icon') + '.svg');
+						// 	// Calculam si afisam temperatura media in acea zi
+						// 	$(this).children('.temp').html(Math.floor(($(xml).find('daily').find('data[' + day + ']').find('temperatureMin')+$(xml).find('daily').find('data[' + day + ']').find('temperatureMax'))/2) + unitText());
+						// });
+					}
 				});
 
-			}, 'jsonp');
+
+
+				// Facem un apel catre API-ul de la Darksky pentru a obtine informatii
+				// legate de vreme cu ajutorul latitudinii si a longitudinii
+				// Precizam unitatea de masura
+				$.get('https://api.darksky.net/forecast/b421d301ce7ef50d4a9d6e02ff0373ed/' + lat +',' + lon + '?units=si', function(weatherInfo) {
+					// Afisam prognoza meteo pentru urmatoarele zile
+					$('.daily .day').each(function(day) {
+						day++;
+
+						// Afisam ziua saptamanii
+						$(this).children('.heading').html(time(weatherInfo.daily.data[day].time));
+						// Afisam icoana specifica prognozei pe acea zi
+						$(this).children('.icon').children('img').attr('src', 'images/icons/' + weatherInfo.daily.data[day].icon + '.svg');
+						// Calculam si afisam temperatura media in acea zi
+						$(this).children('.temp').html(Math.floor((weatherInfo.daily.data[day].temperatureMin+weatherInfo.daily.data[day].temperatureMax)/2) + unitText());
+					});
+
+				}, 'jsonp');
+
+			}
 		});
+
+
+
+
+		// });
 	}
 
 	// Functia apelata in cazul in care coordonatele nu au putut fi preluate
